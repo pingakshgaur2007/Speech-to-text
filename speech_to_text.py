80d347deeecd1276c0c6477aff7cc9753ccc673b
pingakshgaur2007 <73938293+pingakshgaur2007@users.noreply.github.com>
2020-11-13 18:02:15 +0530

    speech_to_text file uploaded

---
+++ speech_to_text.py
@@ -0,0 +1,31 @@
+# pip install SpeechRecognition
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+        print("\nSpeak something...")
+        audio = r.listen(source)
+
+        try:
+            text = r.recognize_google(audio)
+            print("You said : " + text)
+
+        except Exception as e:
+            print(e)
+            print("Error, couldn't recognize your voice.")
+
+def take_my_command():
+    with sr.Microphone() as source:
+        print("\nSpeak something...")
+        audio = r.listen(source)
+
+        try:
+            text = r.recognize_google(audio)
+            print("You said : " + text)
+
+        except Exception as e:
+            print(e)
+            print("Error, couldn't recognize your voice.")
+
+take_my_command()
